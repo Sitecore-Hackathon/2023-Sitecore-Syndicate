7212d2013344a985a4ada270579c18ba42037de0
topazahmed <thisistopaz@hotmail.com>
2023-03-04 15:08:47 -0700

    how to gif and cleanup

--- README.md
+++ README.md
@@ -45,19 +45,9 @@ Sitecore Syndicate
 3. profit
 
 ## Usage instructions
-⟹ Provide documentation about your module, how do the users use your module, where are things located, what do the icons mean, are there any secret shortcuts etc.
 
-Include screenshots where necessary. You can add images to the `./images` folder and then link to them from your documentation:
-
-![Hackathon Logo](docs/images/hackathon.png?raw=true "Hackathon Logo")
-
-You can embed images of different formats too:
-
-![Deal With It](docs/images/deal-with-it.gif?raw=true "Deal With It")
-
-And you can embed external images too:
-
-![Random](https://thiscatdoesnotexist.com/)
+- How it works?
+![how it works](docs/images/sitecore-syndicate-image-generator.gif?raw=true "how it works")
 
 ## Comments
-If you'd like to make additional comments that is important for your module entry.
+Life is short. Don't spend most of your day trying to figure out why init and up script is not working. Look out the window. Enjoy the world and get your architect to do it for you and send you the final script.
--- SitecoreSyndicate/Script/generate-image.ps1
+++ SitecoreSyndicate/Script/generate-image.ps1
@@ -10,88 +10,64 @@
         Topaz Ahmed
 #>
 
-function New-MediaItem
-{
+function New-MediaItem {
     [CmdletBinding()]
-     param(
-             [Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)] [ValidateNotNullOrEmpty()] 
-             [string]$filePath,
+    param(
+        [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)] [ValidateNotNullOrEmpty()] 
+        [string]$filePath,
              
-             [Parameter(Position=1, Mandatory=$true, ValueFromPipeline=$true)] [ValidateNotNullOrEmpty()] 
-             [string]$fileName,
+        [Parameter(Position = 1, Mandatory = $true, ValueFromPipeline = $true)] [ValidateNotNullOrEmpty()] 
+        [string]$fileName,
              
-             [Parameter(Position=2, Mandatory=$true)] [ValidateNotNullOrEmpty()]
-             [string]$mediaPath
-         )
+        [Parameter(Position = 2, Mandatory = $true)] [ValidateNotNullOrEmpty()]
+        [string]$mediaPath
+    )
      
      
     $mco = New-Object Sitecore.Resources.Media.MediaCreatorOptions
     $mco.Database = [Sitecore.Configuration.Factory]::GetDatabase("master");
     $mco.Language = [Sitecore.Globalization.Language]::Parse("en");
     $mco.Versioned = [Sitecore.Configuration.Settings+Media]::UploadAsVersionableByDefault;
-    #$mco.Destination = "$($mediaPath)/$([System.IO.Path]::GetFileNameWithoutExtension($filePath))";
     $mco.Destination = "$($mediaPath)/$fileName"; 
     $mc = New-Object Sitecore.Resources.Media.MediaCreator
     $mc.CreateFromFile($filepath, $mco);
-    
-    write-host 'New-MediaItem'
-    write-host $mco.Destination
-    write-host $filepath
 }
 
 function Wrapper {
     param(
-             [Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)] [ValidateNotNullOrEmpty()] 
-             [string]$source
-         )          
+        [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)] [ValidateNotNullOrEmpty()] 
+        [string]$source
+    )          
     
 
-$Filename = [guid]::NewGuid() #.ToString() + ".png"  #[System.IO.Path]::GetFileName($source)
+    $Filename = [guid]::NewGuid()
  
- Create-Temp-Folder
+    Create-Temp-Folder
 
-$dest = $AppPath + "App_Data\temp\$Filename" + '.png' 
+    $dest = $AppPath + "App_Data\temp\$Filename" + '.png' 
 
-write-host 'Wrapper'
-write-host 'source ----------- ' $source
-write-host 'Filename ----------- ' $Filename
-write-host 'dest ----------- ' $dest
+    Invoke-WebRequest -Uri $source -OutFile $dest
+    $image = Get-Item $dest
 
-
-
-Invoke-WebRequest -Uri $source -OutFile $dest
-$image = Get-Item $dest
-
-#New-MediaItem $dest "$([Sitecore.Constants]::MediaLibraryPath)/Test"
-
-write-host 'before New-MediaItem ----------- '
-write-host $dest
-write-host $Filename
-write-host "$([Sitecore.Constants]::MediaLibraryPath)/Test"
-
-
-New-MediaItem $dest $Filename "$([Sitecore.Constants]::MediaLibraryPath)/Test"
-Remove-Item $dest
+    New-MediaItem $dest $Filename "$([Sitecore.Constants]::MediaLibraryPath)/Test"
+    Remove-Item $dest
 
 }
-
-
 
 
 <#
     .SYNOPSIS
         Making an API call to chat GPT API to get a generated images by a passing phrase
 #> 
-function Get-Image-Urls
-{
+function Get-Image-Urls {
     param(
-        [Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)] [ValidateNotNullOrEmpty()] 
+        [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)] [ValidateNotNullOrEmpty()] 
         [string]$phrase,
         
-        [Parameter(Position=1, Mandatory=$true, ValueFromPipeline=$true)] [ValidateNotNullOrEmpty()] 
+        [Parameter(Position = 1, Mandatory = $true, ValueFromPipeline = $true)] [ValidateNotNullOrEmpty()] 
         [int]$number,
         
-        [Parameter(Position=2, Mandatory=$true, ValueFromPipeline=$true)] [ValidateNotNullOrEmpty()] 
+        [Parameter(Position = 2, Mandatory = $true, ValueFromPipeline = $true)] [ValidateNotNullOrEmpty()] 
         [string]$imageSize
     )
     
@@ -104,9 +80,9 @@ function Get-Image-Urls
     }
     
     $body = @{
-    "prompt" = $phrase
-    "n"      =  [int]$number
-    "size"   = $imageSize 
+        "prompt" = $phrase
+        "n"      = [int]$number
+        "size"   = $imageSize 
     } | ConvertTo-Json
     
     $response = Invoke-RestMethod -Uri $url -Method "POST" -Headers $headers -Body $body
@@ -119,12 +95,11 @@ function Get-Image-Urls
     .SYNOPSIS
         Create a media folder under /sitecore/media library/Images if it doesn't exists 
 #>        
-function Create-Media-Folder
-{
-     param(
-            [Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)] [ValidateNotNullOrEmpty()] 
-            [string]$folderName
-        )
+function Create-Media-Folder {
+    param(
+        [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)] [ValidateNotNullOrEmpty()] 
+        [string]$folderName
+    )
     
     $root = "$([Sitecore.Constants]::MediaLibraryPath)/Images"
     $imagesFolder = Get-Item -Path $root
@@ -132,13 +107,13 @@ function Create-Media-Folder
     $result = "$($imagesFolder.Paths.FullPath)/$adjustedFolderName"
     Write-Host "Images will be stored in '$result' folder"
     if (Test-Path -Path $result) {
-      Write-Host "Destination media folder '$result' already exists"
-      return $result
+        Write-Host "Destination media folder '$result' already exists"
+        return $result
     }
     else {
         # Create the new folder item
         $mediaFolderItem = New-Item -Path $result -ItemType "/sitecore/templates/System/Media/Media folder"
-         # Add some metadata to the new folder
+        # Add some metadata to the new folder
         $mediaLibraryPathItem.Editing.BeginEdit()
         $mediaLibraryPathItem.Fields["__Display name"].Value = $folderName
         $mediaLibraryPathItem.Editing.EndEdit()
@@ -147,101 +122,93 @@ function Create-Media-Folder
 }
 
 function Create-Temp-Folder {
-   $tempFolder = "$AppPath\App_Data\temp"
-   if (Test-Path -Path $tempFolder) {
-      Write-Host "Destination temp folder '$tempFolder' already exists"
-      return $result
+    $tempFolder = "$AppPath\App_Data\temp"
+    if (Test-Path -Path $tempFolder) {
+        Write-Host "Destination temp folder '$tempFolder' already exists"
+        return $result
     }
     else {
         Write-Host "Creating folder '$tempFolder'"
         New-Item -ItemType Directory -Path $tempFolder
     }
-    #AppPath\App_Data\temp
 }
 
-    write-host '------------------------------'
-    #From context menu
-    $selectedItem = Get-Item -Path .
-    $mediaLibraryPathRoot = $selectedItem.Paths.FullPath
+write-host '------------------------------'
+#From context menu
+$selectedItem = Get-Item -Path .
+$mediaLibraryPathRoot = $selectedItem.Paths.FullPath
     
-    write-host 'mediaLibraryPathRoot' $mediaLibraryPathRoot
+write-host 'mediaLibraryPathRoot' $mediaLibraryPathRoot
 
-    $numbers = [ordered]@{    
-      "1"="1"    
-      "2"="2"    
-      "3"="3"    
-      "4"="4"    
-      "5"="5"    
-      "6"="6"    
-      "7"="7"    
-      "8"="8"    
-      "9"="9"    
-      "10"="10"
-    }
+$numbers = [ordered]@{    
+    "1"  = "1"    
+    "2"  = "2"    
+    "3"  = "3"    
+    "4"  = "4"    
+    "5"  = "5"    
+    "6"  = "6"    
+    "7"  = "7"    
+    "8"  = "8"    
+    "9"  = "9"    
+    "10" = "10"
+}
     
-    $options = [ordered]@{    
-     "256x256"="256x256"    
-     "512x512"="512x512"    
-     "1024x1024"="1024x1024"
-    }
+$options = [ordered]@{    
+    "256x256"   = "256x256"    
+    "512x512"   = "512x512"    
+    "1024x1024" = "1024x1024"
+}
 
-    $props = @{    
-        Parameters = 
-        @(        
-          @{Name="phrase"; Prompt = "Enter some text for what you want generated:"}        
-          @{Name="number"; Title="Choose number of images"; Options=$numbers; Tooltip="Choose number of images to generate."}        
-          @{Name="imageSize"; Title="Choose an image size"; Options=$options; Tooltip="Choose image size."}   
-        )    
+$props = @{    
+    Parameters  = 
+    @(        
+        @{Name = "phrase"; Prompt = "Enter some text for what you want generated:" }        
+        @{Name = "number"; Title = "Choose number of images"; Options = $numbers; Tooltip = "Choose number of images to generate." }        
+        @{Name = "imageSize"; Title = "Choose an image size"; Options = $options; Tooltip = "Choose image size." }   
+    )    
           
-        Title = "Generate Images"    
-        Description = "Generate images from Chat GPT."    
-        Width = 300    
-        Height = 300    
-        ShowHints = $true
-    }
+    Title       = "Generate Images"    
+    Description = "Generate images from Chat GPT."    
+    Width       = 300    
+    Height      = 300    
+    ShowHints   = $true
+}
     
-    $dialogResult = Read-Variable @props
+$dialogResult = Read-Variable @props
     
-    if($dialogResult -ne "ok"){
-       Write-Host "Image generation process canceled."
-       Exit
-    }
+if ($dialogResult -ne "ok") {
+    Write-Host "Image generation process canceled."
+    Exit
+}
     
-    Write-Host $phrase
-    Write-Host $number
-    Write-Host $imageSize
+Write-Host $phrase
+Write-Host $number
+Write-Host $imageSize
     
 
-    try 
-    {
-        write-host "Generating $number images based on term '$phrase' and having size of '$imageSize' "   
-        
-        #$phrase = "cat"
-        #$number = 2
-        #$imageSize = "512x512"
-
-        $folder = Create-Media-Folder $phrase
+try {
+    write-host "Generating $number images based on term '$phrase' and having size of '$imageSize' "   
+    $folder = Create-Media-Folder $phrase
 
        
-        $response = Get-Image-Urls $phrase $number $imageSize
-        $imageUrls = $response.data.url
+    $response = Get-Image-Urls $phrase $number $imageSize
+    $imageUrls = $response.data.url
 
-        $i = 1
-        foreach ($imageUrl in $imageUrls) {
-            $imageName = "$SitecoreDataFolder\" + $phrase
-            $imageName = $imageName + "-" + $i
+    $i = 1
+    foreach ($imageUrl in $imageUrls) {
+        $imageName = "$SitecoreDataFolder\" + $phrase
+        $imageName = $imageName + "-" + $i
             
-            write-host '$imageName ' $mediaImagePath
+        write-host '$imageName ' $mediaImagePath
 
-            Wrapper $imageUrl
-            $i = $i + 1
+        Wrapper $imageUrl
+        $i = $i + 1
             
-         write-host 'Done. Check you media folder for the generated images'
-       }
-
+        write-host 'Done. Check you media folder for the generated images'
     }
 
-    catch 
-    {
-        Write-Host "Failed to generate images. Error: $($_.Exception.Message)"
-    }
+}
+
+catch {
+    Write-Host "Failed to generate images. Error: $($_.Exception.Message)"
+}
